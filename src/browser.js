35bb16bcba4db3c977528b9864c07377d31d3ec4
Maxime Biais <maxime.biais@gmail.com>
2018-12-24 10:54:05 +0100

    Enable filters

--- src/browser.js
+++ src/browser.js
@@ -1,22 +1,29 @@
 const Web3 = require('web3');
 const $ = require('jquery');
+const moment = require('moment');
 const subscribeToERC20Events = require('./index');
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
+let filter = '';
 
 // Callback that adds new lines to the table
-function consoleLog(value, symbol, name, from, to, contactAddress) {
-  web3.utils.fromWei(value);
-  const v = web3.utils.fromWei(value).toString();
-  $('#tablebody').prepend(
-    `<tr><td>?</td><td>${symbol}</td><td>${from}</td><td>${to}</td><td>${v}</td></tr>`,
-  );
+function consoleLog(value, symbol, name, from, to, contractAddress) {
+  if (symbol.match(filter) || (symbol.match(filter) || contractAddress.match(filter))) {
+    web3.utils.fromWei(value);
+    const v = web3.utils.fromWei(value).toString();
+    const now = moment().format('LTS');
+    const fromTrunc = from === '0x0' ? from : `${from.slice(0, 16)}...`;
+    const toTrunc = to === '0x0' ? to : `${to.slice(0, 16)}...`;
+    $('#tablebody').prepend(
+      `<tr><td>${now}</td><td><a href="https://etherscan.io/token/${contractAddress}">${symbol}</a></td><td><a href="https://etherscan.io/address/${from}">${fromTrunc}</a></td><td><a href="https://etherscan.io/address/${to}">${toTrunc}</a></td><td>${v}</td></tr>`,
+    );
+  }
 }
 
 $(document).ready(() => {
   // Subscribe to input events
-  $('#search').keypress((event) => {
-    console.log(event.target.value);
+  $('#search').keyup((event) => {
+    filter = event.target.value;
   });
 
   // Subscribe to web3 events
--- src/cli.js
+++ src/cli.js
@@ -3,11 +3,11 @@ const subscribeToERC20Events = require('./index');
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
 
-function consoleLog(value, symbol, name, from, to, contactAddress) {
+function consoleLog(value, symbol, name, from, to, contractAddress) {
   const strValue = value.toString();
   // eslint-disable-next-line no-console
   console.log(
-    `${strValue} ${symbol} (${name}) sent from ${from} to ${to} - contract address: ${contactAddress}`,
+    `${strValue} ${symbol} (${name}) sent from ${from} to ${to} - contract address: ${contractAddress}`,
   );
 }
 
